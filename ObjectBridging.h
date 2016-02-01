c47ec66277e14822d9cf318023c4a840ab772445
wczhang <wczhang@zhaidou.com>
2016-02-01 17:40:25 +0800

    同步代码

---
+++ ObjectBridging.h
@@ -0,0 +1,15 @@
+//
+//  ObjectBridging.h
+//  EasemobDemo
+//
+//  Created by zhangweicheng on 16/2/1.
+//  Copyright © 2016年 zhangweicheng. All rights reserved.
+//
+
+#ifndef ObjectBridging_h
+#define ObjectBridging_h
+
+
+#endif /* ObjectBridging_h */
+
+#import <EaseMobSDKFull/EMSDKFull.h>
