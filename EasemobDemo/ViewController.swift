3e9b32c54e85fef2421efa512516135134114729
wczhang <wczhang@zhaidou.com>
2016-02-01 16:18:03 +0800

    Initial Commit

---
+++ EasemobDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasemobDemo
+//
+//  Created by zhangweicheng on 16/2/1.
+//  Copyright © 2016年 zhangweicheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
